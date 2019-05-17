018af99a71a658de81ba420e63f03ba0b63a52e8
vishaag <vishaag@gmail.com>
2019-05-18 05:03:58 +0530

    Initial

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+var Jimp = require('jimp');
+fs = require('fs');
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  Jimp.read('./philosoraptor.jpg')
+  .then(image => {
+    Jimp.loadFont(Jimp.FONT_SANS_64_WHITE).then(font => {
+      image.print(font, 10, 10, 'Hello worlds!');
+      image.print(font, 10, 300, 'Hello worlds!');
+      image.write('phil.jpg', 
+        function () {
+          img = fs.readFileSync('phil.jpg');
+          fs.unlinkSync('phil.jpg')
+          res.writeHead(200, {'Content-Type': 'image/jpg' });
+          res.end(img, 'binary');
+        }
+      )
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+});
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+var Jimp = require('jimp');
+fs = require('fs');
+
+
+module.exports = (req, res) => {
+  Jimp.read(__dirname + '/philosoraptor.jpg')
+  .then(image => {
+    Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => {
+      image.print(font, 10, 10, 'Hello worlds!');
+      image.write('/tmp/phil.jpg', 
+        function () {
+          img = fs.readFileSync('/tmp/phil.jpg');
+          fs.unlinkSync('/tmp/phil.jpg')
+          res.writeHead(200, {'Content-Type': 'image/jpg' });
+          res.end(img, 'binary');
+        }
+      )
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
+};
